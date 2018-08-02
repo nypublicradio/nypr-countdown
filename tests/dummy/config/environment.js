2085eea13017162a134b4b8e702328ca4d05fe51
Brian Whitton <me@brianwhitton.com>
2018-08-02 11:49:23 -0400

    couple more deploy configs

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.rootURL = '/nypr-countdown';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
